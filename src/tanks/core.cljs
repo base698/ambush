aa629542b8bdbff2c5b7276b33698e6e474207d4
Justin Thomas <justin.thomas1@gmail.com>
2014-04-14 20:51:58 -0700

    slow going

--- src/tanks/core.cljs
+++ src/tanks/core.cljs
@@ -1,23 +1,41 @@
-(ns tanks.core (:require [tanks.utils :as utils]))
+(ns tanks.core
+ (:use [tanks.utils :only [log]]))
+
 (def world { 
   :width 800 
   :height 600 
-  :players []
+  :entities []
 })
 
 (def player {
   :score 0
   :health 100
   :position [10,10]
+  :angle 0
+  :velocity 0
   :bombs 1})
 
-(defn hook-events []
-  (let [body (.getElementById js/document "main")]
-    (utils/log body)
-  (.addEventListener body "keypress" (fn [& args] )
-    (utils/log player)
-  )))
+(def shot {
+  :angle 0
+  :velocity 0
+  :position [0,0]
+})
 
-(defn ^:export main [canvas]
-  (hook-events))
+(def key-map { 87 :w 83 :s 65 :a 68 :d 32 :space})
+
+(defn draw-frame [world]
+  (let 
+    [canvas (.getElementById js/document "main") ctx (.getContext canvas "2d")]
+   ))
+
+
+(defn ^:export main []
+  (def keypresses {})
+  (let [body (.getElementById js/document "main")]
+    (.setInterval js/window #(log keypresses) 2000)
+    (.addEventListener js/window "keydown" 
+      #(set! keypresses 
+          (assoc keypresses 
+            (key-map (aget %1 "keyCode")) true)))
+    (.addEventListener js/window "keyup" (fn [event] ))))
 
