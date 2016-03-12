f83a2a6c0050fe5699c0417fc6387442edcc36dc
Justin Thomas <justin.thomas1@gmail.com>
2016-03-11 17:18:16 -0800

    rewriting collision detection/oob

--- src-cljs/tanks/core.cljs
+++ src-cljs/tanks/core.cljs
@@ -1,16 +1,17 @@
 (ns tanks.core
  (:require [tanks.utils :refer [log]]))
 
+(enable-console-print!)
+
 (def WIDTH 800)
 (def HEIGHT 600)
-(def world { 
+
+(defonce world { 
   :width WIDTH
   :height HEIGHT
 })
 
 (def x 1)
-
-(enable-console-print!)
 
 (def tank-id 1)
 
@@ -18,16 +19,29 @@
   (set! tank-id (inc tank-id)) tank-id)
 
 (def first-id (get-id))
-(def entities {first-id {:id first-id :type :ant :position [400, 400]}})
 
-(def player (atom {:type :player
-                   :score 0
-                   :color "#072"
-                   :health 100
-                   :position [200,200]
-                   :angle 0
-                   :last-shot 0
-                   :bombs 1}))
+(defonce events (atom #{}))
+
+(defn get-player [p c]
+  {:id (get-id)
+   :type :player
+   :score 0
+   :color c
+   :health 100
+   :position p
+   :angle 0
+   :last-shot 0
+   :bombs 1})
+
+(def player (atom (get-player [200, 200] "#072")))
+
+(def entities (atom {first-id
+                     {:id first-id
+                      :type :ant
+                      :position [400, 400]}}))
+
+
+;;(defonce players (atom [player]))
 
 (defonce key-map { 87 :w 
                    83 :s 
@@ -35,12 +49,20 @@
                    68 :d 
                    32 :space})
 
-
 (defn add-ant []
   (let [id (get-id) ]
-    (set! entities 
-      (assoc entities 
-        id {:id id :type :ant :position [(rand-int 400) (rand-int 400)]}))))
+    (swap! entities 
+      assoc entities 
+      id {:id id
+          :type :ant
+          :position [(rand-int 400) (rand-int 400)]})))
+
+;; todo finish
+(defn add-player []
+  (let [player (get-player [500 500] "#622")
+        id (player :id)]
+    (swap! entities assoc id player)))
+
 
 (defonce player-key-map {:space {:shoot true} 
                          :w {:player-move 1.1} 
@@ -52,7 +74,8 @@
     (+ (* c (/ t d) (/ t d)) b))
 
 (defn get-ctx [] 
-  (let [canvas (.getElementById js/document "screen") ctx (.getContext canvas "2d")]
+  (let [canvas (.getElementById js/document "screen")
+        ctx (.getContext canvas "2d")]
   ctx))
 
 (defmulti draw-entity :type)
@@ -60,11 +83,14 @@
 (defmethod draw-entity :shrapnel [p])
 
 (defmethod draw-entity :player [p]
-  (let [ctx (get-ctx) position (p :position) x (first position) y (second position)]
+  (let [ctx (get-ctx)
+        position (p :position)
+        x (first position)
+        y (second position)]
     (.save ctx)
-    (set! (.-fillStyle ctx) (@player :color))
+    (set! (.-fillStyle ctx) (p :color))
     (.translate ctx (+ x  (/ 20 2)) (+ y (/ 20 2)))
-    (.rotate ctx (@player :angle))
+    (.rotate ctx (p :angle))
     (.fillRect ctx -10 -10 20 20)
     (.fillRect ctx 8 -2 5 5)
     (.restore ctx)))
@@ -91,9 +117,6 @@
 (defn atan2 [a] (.atan2 js/Math a))
 (defn cos [a] (.cos js/Math a))
 
-(defn choose [a b id] 
-   (if (= id (a :id)) b a))
-
 (defn in-bounds? [x y] 
   (and (>= x -10) (< x WIDTH) (>= y -10) (< y HEIGHT)))
 
@@ -106,19 +129,10 @@
 (defn between? [a b]
    (and (>= a b) (<= a (+ b 5))))
 
-(defn detect-hit [x y] 
-  (find-first (fn [e] 
-    (let [pos (e :position) aX (first pos) aY (second pos)]
-    (and (or (between? x aX) (between? (+ x 5) aX)  )
-    (or (between? y aY) (between? (+ y 5) aY)  ))
-    )) (filter #(= :ant (%1 :type)) (vals entities))))
-
-(defn remove-entities [ids]
-  (set! entities (apply (partial dissoc entities) ids)))
-
-(defn increase-score [] (swap! player update-in [:score] inc))
+(defn inc-score [] (swap! player update-in [:score] inc))
 
 (defn get-y [ent] (second (:position ent)))
+
 (defn get-x [ent] (first (:position ent)))
 
 (defmulti do-event #(first (keys %1)))
@@ -128,29 +142,22 @@
 ; {:ant-move 3, :id 2}
 (defmethod do-event :ant-move [e]
   (let [id (e :id) 
-        ant (entities id) 
+        ant (@entities id) 
         deltaY (- (get-y ant) (get-y @player))
         deltaX (- (get-x ant) (get-x @player))
         angle (* (/ 180 PI) (atan (/ deltaY deltaX)))
         position (ant :position)
         x (+ (first position) (* (cos angle) 1 ))
         y (+ (second position) (* (sin angle) 1 ))]
-    (set! entities (assoc entities id (assoc ant :position [x y])))))
+    (swap! entities assoc-in [id :position] [x y])))
 
 (defmethod do-event :shot-move [e]
     (let [move (e :shot-move)
-      shot (entities (e :id))]
-      (cond shot
-        (let [position (shot :position)
-              x (+ (first position) (* (cos (shot :angle)) move ))
-              y (+ (second position) (* (sin (shot :angle)) move ))
-              new-shot (assoc shot :position [x y])
-              hit-ant (apply detect-hit (new-shot :position))]
-        (if hit-ant (do (remove-entities [(hit-ant :id) (new-shot :id)]) (increase-score))
-            (let [in-bounds-entities (keep #(let [pos (%1 :position) shotX (first pos) shotY (second pos)] 
-          (if (in-bounds? shotX shotY ) {(%1 :id) (if (= (new-shot :id) (%1 :id)) new-shot %1)}) ) (vals entities))]
-        (set! entities (apply merge (cons entities in-bounds-entities)))))))))
-
+          shot (@entities (e :id))
+          position (shot :position)
+          x (+ (first position) (* (cos (shot :angle)) move ))
+          y (+ (second position) (* (sin (shot :angle)) move ))]
+          (swap! entities assoc-in [(e :id) :position] [x y])))
 
 (defmethod do-event :player-turn [e]
   (let [angle (e :player-turn)]
@@ -165,33 +172,35 @@
         new-position [(+ x (first old-position)) (+ y (second old-position)) ]]
     (player-position! new-position)))
 
-(defmethod do-event :shoot [x]
-  (let [t (x :timestamp) x (first (@player :position)) y (second (@player :position))]
+(defmethod do-event :shoot [e]
+  (let [t (e :timestamp)
+        x (get-in @player [:position 0])
+        y (get-in @player [:position 1])]
     (when (can-shoot? t) 
-    (let [id (get-id)]
-      (set! entities (assoc entities id {
-        :id id 
-        :type :shot 
-        :angle (@player :angle) 
-        :position [(+ 7 x) (+ 7 y)]
-      })))
-    (player-field! :last-shot t)
-    )))
+      (let [id (get-id)]
+        (swap! entities assoc id {
+          :id id 
+          :type :shot 
+          :from-player (@player :id)
+          :angle (@player :angle) 
+          :position [(+ 7 x) (+ 7 y)]}))
+        (player-field! :last-shot t))))
 
 (defn draw-world []
   (let [ctx (get-ctx)]
     (.clearRect ctx 0 0 800 600)
     (do (draw-entity @player)
-        (doseq [x (vals entities)]
+        (doseq [x (vals @entities)]
           (do (draw-entity x))))))
 
+; these are events that just happen 
 (defn get-world-events [timestamp] 
-   (map (fn [ent] ({
-                    :shot {:shot-move 4
-                           :id (ent :id)}
-                    :ant {:ant-move 3
-                          :id (ent :id)}
-   } (ent :type))) (vals entities)))
+  (map (fn [ent]
+         ({:shot {:shot-move 4
+                  :id (ent :id)}
+           ; :ant {:ant-move 3
+           ; :id (ent :id)}
+   } (ent :type))) (vals @entities)))
 
 (defn handle-events [events] 
   (loop [x events]
@@ -199,29 +208,52 @@
       (if (empty? x) nil
       (recur (rest x)))))
 
+;; see if another player was struck
+(defn detect-hits [shots]
+  (let [x 1]
+    ;(prn shots)
+    ))        
+
+(defn get-shots [entities]
+  (filter #(= :shot (%1 :type)) (vals entities)))
+
+(defn oob? [p]
+  (let [[x y] p]
+    (or (> x WIDTH)
+        (> y HEIGHT)
+        (< x 0)
+        (< y 0))))
+
+(defn detect-shot-oob [shots]
+  (let [oob (filter #(oob? (%1 :position)) shots)]
+    (apply (partial swap! entities dissoc)
+           (map :id oob))))        
+
+
 (defn update-world [keypresses timestamp]
   (let [press-list (seq
                     (select-keys
                      keypresses
                      (for [[k v] keypresses :when v] k)))
-          player-events (->> press-list
-                             (map #(player-key-map (first %1)))
-                             (map #(assoc %1 :timestamp timestamp)))
-                             world-events (get-world-events timestamp)]
-                             (handle-events (concat player-events world-events))))
-
-(defn explosion-at [x y]
-  )
+        player-events (->> press-list
+                           (map #(player-key-map (first %1)))
+                           (map #(assoc %1 :timestamp timestamp)))
+        world-events (get-world-events timestamp)]
+    (do 
+        ;; TODO collision detection
+        (handle-events player-events)
+        (handle-events world-events)
+        (detect-hits (get-shots @entities))
+        (detect-shot-oob (get-shots @entities)))))
 
   
 (defn main []
-    ;(let [keypresses (atom {})]
     ; (.setInterval js/window add-ant 8000)
     (let [keypresses (atom {})]
         ((fn render-loop [timestamp]
-            (update-world @keypresses timestamp) (draw-world)
+           (update-world @keypresses timestamp)
+           (draw-world)
             (.requestAnimationFrame js/window render-loop)))
-
         (.addEventListener js/window "keydown"
             #(swap! keypresses
                     assoc
