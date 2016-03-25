36f665fa792651381030d2fdba7686932add770a
Justin Thomas <justin.thomas1@gmail.com>
2016-03-25 16:43:14 -0400

    add multiple builds

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject tanks "0.0.3"
-  :plugins [[lein-figwheel "0.5.0-6"]]
+  :plugins [[lein-figwheel "0.5.0-6"]
+            [lein-cljsbuild "1.1.3"]]
   :clean-targets [:target-path "out"]
   :dependencies [
     [org.clojure/clojure "1.8.0"]
@@ -7,16 +8,25 @@
     [cljs-hash "0.0.2"]
     [org.clojure/clojurescript "1.7.228"]]
   :cljsbuild {
-    :builds [{
-        :id "dev"
-        ; The path to the top-level ClojureScript source directory:
-        :source-paths ["src-cljs"]
-        :figwheel true
-        ; The standard ClojureScript compiler options:
-        ; (See the ClojureScript compiler documentation for details.)
-        :compiler {
-          :output-to "resources/public/main.js"  
-          :asset-path "out"
-          :main "tanks.core"
-          :optimizations :none
-          :pretty-print true}}]})
+              :builds {:prod {:id "prod"
+                        ; The path to the top-level ClojureScript source directory:
+                        :source-paths ["src-cljs"]
+                        ; The standard ClojureScript compiler options:
+                        ; (See the ClojureScript compiler documentation for details.)
+                        :compiler {
+                        :output-to "resources/public/main.js"  
+                        :asset-path "out"
+                        :main "tanks.core"
+                        :optimizations :advanced}}
+                       :dev {:id "dev"
+                        ; The path to the top-level ClojureScript source directory:
+                        :source-paths ["src-cljs"]
+                        :figwheel true
+                        ; The standard ClojureScript compiler options:
+                        ; (See the ClojureScript compiler documentation for details.)
+                        :compiler {
+                        :output-to "resources/public/main.js"  
+                        :asset-path "out"
+                        :main "tanks.core"
+                        :optimizations :none
+                        :pretty-print true}}}})
